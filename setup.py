f6d3cd6215c44194594de6a7f128baf1a6a27081
dnaextrim <dna.extrim@gmail.com>
2016-11-01 16:13:44 +0700

    Fixing Installation Bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='django_adminlte_x',
     version='0.3',
-    packages=['django_adminlte_x'],
+    packages=['adminlte'],
     license='MIT License',
     description='AdminLTE Bootstrap Theme packaged for Django',
     long_description=open('README.md', 'r').read(),
@@ -11,8 +11,5 @@ setup(
     author='Dony Wahyu Isprananda',
     author_email='dna.extrim@gmail.com',
     classifiers=[],
-    keywords='django bootstrap adminlte theme',
-    install_requires=[
-        'django>=1.9',
-    ],
+    keywords='django bootstrap adminlte theme'
 )
