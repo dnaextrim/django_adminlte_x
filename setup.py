33f00a0aa6b028489d0a03bdb76a73664ec92c23
dnaextrim <dna.extrim@gmail.com>
2016-11-02 08:29:54 +0700

    Fixing Bug Installation

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
-from setuptools import setup
+import os
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_adminlte_x',
-    version='0.6',
-    packages=['django_adminlte_x'],
+    version='0.7',
+    packages=find_packages(),
     license='MIT License',
     description='AdminLTE Bootstrap Theme packaged for Django',
     long_description=open('README.md', 'r').read(),
     url='https://github.com/dnaextrim/django_adminlte_x',
     author='Dony Wahyu Isprananda',
     author_email='dna.extrim@gmail.com',
-    classifiers=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
     keywords='django bootstrap adminlte theme'
 )
