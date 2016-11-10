7635a1227099d7f98e1a8bb1841edfb9a7b9129a
dnaextrim <dna.extrim@gmail.com>
2016-11-10 12:48:14 +0700

    up version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __import__("adminlte")
 
 setup(
     name='django-adminlte-x',
-    version='0.15',
+    version='0.16',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
